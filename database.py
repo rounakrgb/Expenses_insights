3e81531bb81388f3d37cde341acc50c904a32192
rounakrgb <155313656+rounakrgb@users.noreply.github.com>
2026-02-11 15:08:59 +0530

    Refactor DATABASE_URL to use environment variable
    Updated DATABASE_URL to use environment variable with a fallback.

--- database.py
+++ database.py
@@ -3,9 +3,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL="postgresql://lap52:123@localhost:5432/expense_db"
-
-
+DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:postgres@localhost:5432/expense_db"
+)
 
 if DATABASE_URL.startswith("sqlite"):
     engine = create_engine(
@@ -14,7 +15,6 @@ if DATABASE_URL.startswith("sqlite"):
     )
 else:
     engine = create_engine(DATABASE_URL, echo=True)
-
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
